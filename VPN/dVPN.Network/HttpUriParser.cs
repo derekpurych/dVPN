65046e9dcdb72b46b0779713ef051322bd0a9e38
derekpurych <164971313+derekpurych@users.noreply.github.com>
2024-08-16 02:17:09 -0700

    Delete VPN directory

--- VPN/dVPN.Network/HttpUriParser.cs
+++
@@ -1,8 +0,0 @@
-using System
-
-public class HttpStyleUriParser : UriParser
-
-    protected virtual string GetComponents (Uri uri, UriComponents components, UriFormat format);
-    protected virtual void InitializeAndValidate (Uri uri, out UriFormatException? parsingError);
-    public virtual bool Equals (object? obj);
-
